a9da9e0ffb237fdc3172bcb194a054dfd864f41a
Ankit <ankit.stackmaze@gmail.com>
2019-02-26 18:29:50 +0530

    complate pages

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+// for responsive slider
 $('.responsive').slick({
   dots: true,
 	prevArrow: $('.prev'),
 	nextArrow: $('.next'),
   infinite: true,
-  autoplay: false,
+  autoplay: true,
+  arrows: false,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 3,
@@ -36,3 +38,98 @@ $('.responsive').slick({
     // instead of a settings object
   ]
 });
+// for toogle menu
+$(document).foundation();
+  function myFunction(x) {
+  x.classList.toggle("change");
+  }
+
+$(document).ready(function(){
+  $(".toogle-menu").click(function(){
+    $('.btm-header-content ul').animate({
+      width: "toggle"
+  });
+  });
+});
+   
+// slider-articleformate-page
+$(document).ready(function(){
+  
+// SLIDER-two
+  $('.slider-heading').slick({
+    dots: true,
+    speed: 1000,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: '<div class="slick-custom-arrow slick-custom-arrow-right"><i class="fas fa-angle-right"></i></div>',
+    prevArrow: '<div class="slick-custom-arrow slick-custom-arrow-left"><i class="fa fa-angle-left"></i></div>',
+  });
+
+});
+
+// for video slider script    
+  $(document).ready(function(){
+    $('.carousel').slick({
+    slidesToShow: 1,
+    dots:true,
+    nextArrow: '<div class="slick-custom-arrow slick-custom-arrow-right"><i class="fas fa-angle-right"></i></div>',
+    prevArrow: '<div class="slick-custom-arrow slick-custom-arrow-left"><i class="fa fa-angle-left"></i></div>',
+    infinite: true,
+    centerMode: false,
+    });
+  });
+  
+  // click to up and down menu for (about us page)
+  $('.click_advance').click(function(){
+    $('.display_advance').toggle('1000');
+    icon = $(this).find("i");
+    icon.toggleClass("fas fa-angle-up fas fa-angle-down")
+   })
+
+  $('.click-t').click(function(){
+    $('.display-t').toggle('1000');
+    icon = $(this).find("i");
+    icon.toggleClass("fas fa-angle-up fas fa-angle-down")
+  })
+
+  $('.click_advance-one').click(function(){
+    $('.display_advance-one').toggle('1000');
+    icon = $(this).find("i");
+    icon.toggleClass("fas fa-angle-up fas fa-angle-down")
+  })
+
+  $('.click_advance-second').click(function(){
+    $('.display_advance-second').toggle('1000');
+    icon = $(this).find("i");
+    icon.toggleClass("fas fa-angle-up fas fa-angle-down")
+  })
+
+  $('.click_advance-third').click(function(){
+    $('.display_advance-third').toggle('1000');
+    icon = $(this).find("i");
+    icon.toggleClass("fas fa-angle-up fas fa-angle-down")
+  })
+// CHARTS 
+  if ($('#myChart').length){
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3],
+            backgroundColor: [
+              '#003D6B',
+              '#990000',
+              '#8FA3AD'
+            ],
+            borderWidth: 0
+        }]
+      },
+      options: {
+        responsive: true,
+        cutoutPercentage: 90,
+      }
+    });
+  }
